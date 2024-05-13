7965812ef855ed9d0a1dc15ee1f67e1d0e6ba2c1
Achintya-G <achintya.g.available@gmail.com>
2024-05-13 19:55:52 +0530

    comment

--- cli.py
+++ cli.py
@@ -1,5 +1,3 @@
-import os
-import json
 from cliFuncs import *
 
 
@@ -11,27 +9,27 @@ def assigner(function):
 if __name__ == "__main__":
     # Checking if program is being run for first time and running initialize function
     if not os.path.isfile(os.getcwd()+"\\settings.json"):
-        username = input("\tEnter Your name : \n\t")
-        settings = {"name": username}
-        with open("settings.json", "w") as f:
-            json.dump(settings, f, indent=4)
-    else:
-        with open("settings.json","r") as f:
-            name = json.load(f)["name"]
-        print(f"\t Welcome back {name}")
+        initialize()
+
+    # Greeting user
+    greet()
 
     # running a loop to continuously take input
     while True:
         result = None
+
         # taking input and removing trailing/leading spaces and making into a list
         n = input(">>> ")
         n = n.strip().split(" ")
+
         # Checking if first word is a function keyword
         if n[0] in func_list:
             func = assigner(n[0])
+
             # Checking if additional parameters are being passed onto function
             if n[1:] is not None:
                 result = func(n[1:])
+
             # Checkin if result from function is not null and printing
             if result is not None and result != []:
                 print("\t", result)
--- cliFuncs.py
+++ cliFuncs.py
@@ -1,10 +1,29 @@
 import json
-
-def firstRun():
-    pass
-
+import os
+import sqlite3
 
 
+# Program Functions
+def initialize():
+    username = input("\tEnter Your name : \n\t")
+    settings = {"name": username}
+    with open("settings.json", "w") as f:
+        json.dump(settings, f, indent=4)
+    if not os.path.isfile(os.getcwd()+"SongLibrary.db"):
+        song_db = sqlite3.connect("SongLibrary.db")
+        cur = song_db.cursor()
+        cur.execute("CREATE TABLE Tracks(Track_ID INT,Track_Title CHAR,Track_Address CHAR,)")
+        cur.execute("CREATE TABLE Albums(Album_ID INT,Album_Name CHAR,)")
+        cur.execute("CREATE TABLE Artists(Artist_ID INT,Artist_Name CHAR,)")
+        cur.execute("CREATE TABLE Main(Track_ID INT,Artist_ID CHAR,Album_ID INT)")
+
+def greet():
+    with open("settings.json","r") as f:
+        name = json.load(f)["name"]
+    print(f"\t Welcome back {name}")
+
+
+# User Accessible Functions
 def echo(*x):
     """Echos anything passed to this function"""
     for i in x[0]:
@@ -23,6 +42,7 @@ def cliHelp(x):
             print("Invalid argument \n\t\"", x[0], "\" Not a valid function name", sep="")
             return
     print("pass a function as an argument to see how it must be used")
+
 
 
 func_list = {"echo": echo,
